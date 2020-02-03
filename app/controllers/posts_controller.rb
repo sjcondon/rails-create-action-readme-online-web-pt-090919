bd0c8d78e57aa4271a9c58c96ae029bf77702fd9
Sarah Condon <s.j.condon01@gmail.com>
2020-02-03 16:43:59 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,11 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
-  # add create method here
+  
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+  end
 end
